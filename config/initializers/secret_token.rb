8c6fb57b5fa2bd50635f62df039e6a56f0d7e45e
lgorse <lgorse@stanford.edu>
2013-10-28 23:36:46 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Olympos::Application.config.secret_token = '7d1591a00bc80eb37550da9558f9faf4a49447e1e52dd6c142e6f18a124839d537d0054202ee7c74b704ff4278c7347bc3ba0d6aee6a348717f6563508380cca'
