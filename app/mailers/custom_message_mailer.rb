8286ce6e312dd2d3e715fe548c01b2b4d89a20d3
lgorse <lgorse@stanford.edu>
2013-11-17 22:17:40 -0800

    implemented e-mail html for messaging

--- app/mailers/custom_message_mailer.rb
+++ app/mailers/custom_message_mailer.rb
@@ -1,10 +1,15 @@
 class CustomMessageMailer < ActionMailer::Base
+  include MailerHelper
+
   default from: DEFAULT_FROM
 
   def send_email(receipt, recipient)
   	@message = receipt.message
+  	@sender = @message.sender
   	@recipient = recipient
-  	mail(:to => "olympos.help@gmail.com", :subject => "Hi bro what is up?")
+  	@conversation = @message.conversation
+  	@host = parse_host
+  	mail(:to => "olympos.help@gmail.com", :subject => "#{@sender.firstname} sent you a message on #{APP_NAME}")
   end
 
 
