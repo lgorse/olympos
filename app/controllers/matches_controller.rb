9192f5fab97237968e54e2420882a1d9444962f3
lgorse <lgorse@stanford.edu>
2013-11-26 15:56:19 -0800

    implemented match basic form with the layer selector

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -7,5 +7,8 @@ class MatchesController < ApplicationController
 	end
 
 	def create
+		@player1_score = params[:match][:player1_score].map {|key, value| value.to_i unless value.blank?}.compact
+		@player2_score = params[:match][:player2_score].map {|key, value| value.to_i unless value.blank?}.compact
+		@match = Match.create(params[:match].merge(:player1_score => @player1_score, :player2_score => @player2_score, :player1_confirm => true))
 	end
 end
--- spec/controllers/matches_controller_spec.rb
+++ spec/controllers/matches_controller_spec.rb
@@ -33,4 +33,38 @@ describe MatchesController do
 
 	end
 
+	describe "POST 'create'" do
+		before(:each) do
+			@winner = FactoryGirl.create(:user)
+			@loser = FactoryGirl.create(:user)
+			test_sign_in(@winner)
+		end
+
+		describe "if successful" do
+			before(:each) do
+				@attr = {:player1_id => @winner.id, :player2_id => @loser.id,
+						 :player1_score=>{"0"=>"11", "1"=>"12", "2"=>"11", "3"=>"", "4"=>""}, 
+						 :player2_score=>{"0"=>"4", "1"=>"10", "2"=>"4", "3"=>"", "4"=>""}
+						}
+
+			end
+
+			it 'should create a new match' do
+				lambda do
+					post :create, :match => @attr
+				end.should change(Match, :count).by(1)
+			end
+
+		end
+
+		describe "if failed" do
+
+			it "should render the new template" do
+
+			end
+
+		end
+
+	end
+
 end
