02b6e591abf6b227eada5c3ed0b0e10f19babf28
lgorse <lgorse@stanford.edu>
2013-12-15 22:51:14 -0800

    added open graph tags to application

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 
 
 	def new
-		redirect_to home_users_path if session[:user_id]
+		redirect_to search_users_path if session[:user_id]
 	end
 
 	def create
